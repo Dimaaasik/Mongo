936e0b54580b410d684559f08e8b28f578187785
Dimaaasik <nochevca.dimangio@gmail.com>
2023-04-05 02:00:34 +0300

    интегрирую mongoose

---
+++ project-mongo/company.js
@@ -0,0 +1,27 @@
+const mongose = require('mongoose');
+
+const Schema = mongose.Schema
+
+const companySchema = new Schema({
+    founder:{
+        type: String,
+        required: true,
+    },
+    workers:{
+        type: Number,
+        required: true,
+    },
+    year:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    }
+    }
+)
+
+const Company = mongose.model('company', companySchema)
+
+module.exports = Company
