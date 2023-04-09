3499a586c8b039450dc6a6451c80f4893dbfe121
Dimaaasik <nochevca.dimangio@gmail.com>
2023-04-09 06:20:32 +0300

    Лр 10 Танцы с бубном из-за не рабочего метода

--- project-mongo/server.js
+++ project-mongo/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-//const { connectToDb, getDb } = require('./db.js');
 const mongoose = require('mongoose')
+const {ObjectId} = require('mongodb')
+
+
 const Company = require('./company')
 
 const PORT = 3000;
 const URL = 'mongodb://localhost:27017';
-//const dbName = 'Search';
+
 
 const app = express();
 app.use(express.json());
@@ -13,26 +15,23 @@ app.use(express.json());
 
 mongoose
     .connect(URL)
-    .then(() => console.log('Connected To DB'))
+    .then(() => console.log('Connected To DB?!'))
     .catch((err) => console.log(`DB connection error ${err}`))
+
+app.listen(PORT, (err) => {
+            err ? console.log(err) : console.log(`Server works on port ${PORT}`);
+        });
 
 const handleError = (res, error) => {
     res.status(500).json({error});
+    console.log(error)
 }
 
 let db;
-connectToDb((err) => {
-    if (!err) {
-        app.listen(PORT, (err) => {
-            err ? console.log(err) : console.log(`Server works on port ${PORT}`);
-        });
-        db = getDb();
-    } else {
-        console.log(`Connection error ${err}`);
-    }
-});
+
 app.post('/companies', (req, res) => {
     const company = new Company(req.body);
+
     company
         .save()
         .then((result) =>{
@@ -40,23 +39,7 @@ app.post('/companies', (req, res) => {
                 .status(201)
                 .json(result);
         })
-        .catch(() => { handleError (res, "Something went wrong")})
+        .catch(() => { handleError (res, `Something went wrong ${res}`)})
 })
 
-
-/*
-app.post('/companies', (req, res) =>{
-    console.log('POST')
-    db
-        .collection('companies')
-        .insertOne(req.body)
-        .then((result) => {
-            res
-                .status(201)
-                .json(result);
-        })
-        .catch(() => { handleError (res, "Something went wrong")})
-
-});
-*/
 
